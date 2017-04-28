1e3b4ce7053d0b950a33470259e0d968b85a001c
haantje0 <l.d.haan@outlook.com>
2017-04-28 13:16:19 +0200

    Add files via upload

--- untitled9.py
+++ untitled9.py
@@ -5,13 +5,16 @@ Created on Wed Apr 19 13:59:41 2017
 @author: lex
 """
 
+import matplotlib.pyplot as plt
 import time
 import numpy as np
 from copy import deepcopy
+from random import randint
 
 start_time = time.time()
 
-proteinstring = "HHPHHHPH"
+proteinstring = "HPHPPHHPPHHPPPHPHPHPHPHPHHPH"
+
 protein = []
 
 for i in proteinstring:
@@ -23,47 +26,29 @@ gridlength = 2 * lenprotein + 1
 
 grid = np.chararray((gridlength, gridlength))
 
+Hbonds = []
+
 directions = []
 
-bestproteins = [{'grid': grid, 'score': 0}]
-
-for i in protein[0:-1]:
+for i in range(lenprotein - 1):
     directions.append(0)
 
-def recursion(n):
-    if len(directions) - n > 0:
-        recursion(n + 1)
-        for i in range(4):
-            
-            if directions[n] != 3:
-                directions[n] += 1                
-                proteinplacer()
-                recursion(n + 1)
-                
-            else:
-                directions[n] = 0
-                return 
+coordinates = [{'coordinate': ([0,0]),'letter': ""}]
 
+bestproteins = [{'score': 0, 'coordinates': coordinates}]
 
-def function(n):
-    if n >= 1:
-        for i in range(4):
-            
-            if directions[n] != 3:
-                directions[n] += 1
-                proteinplacer()
-                recursion(n + 1)
-                
-            else:
-                directions[n] = 0
-                function(n - 1)
-    return
+#for i in protein[0:-1]:
+#    directions.append(0)
 
-
-def proteinplacer():
+def proteinchecker():
             
     global bestproteins  
-        
+    global coordinates
+    global Hbonds
+    
+    del Hbonds[:]
+    del coordinates[:]
+    
     grid.fill("")
     
     score = 0    
@@ -72,6 +57,9 @@ def proteinplacer():
 
     x = lenprotein
     y = lenprotein
+    
+    coordinates.append({'coordinate': ([x,y]),'letter': protein[0]})
+    
     for i in range(len(directions)):
         
         if directions[i] == 0:
@@ -85,20 +73,21 @@ def proteinplacer():
 
         if grid[y, x] == "":
             grid[y, x] = protein[i + 1]
+            coordinates.append({'coordinate': ([x,y]),'letter': protein[i + 1]})
         else:
-            return
+            return -1
         
         if protein[i + 1] == "H":
             score += givescore(directions[i], x, y)
 
-    
     if bestproteins[0]['score'] == score:
-        bestproteins.append({'grid': deepcopy(grid), 'score': score})
+        if not any (d['coordinates'] == coordinates for d in bestproteins):
+            bestproteins.append({'score': score, 'coordinates': coordinates})
     elif bestproteins[0]['score'] < score:
         del bestproteins[:]        
-        bestproteins.append({'grid': deepcopy(grid), 'score': score})
+        bestproteins.append({'score': score, 'coordinates': coordinates})
     
-    return
+    return score
 
 
 def givescore(direction, x, y):
@@ -107,29 +96,69 @@ def givescore(direction, x, y):
     if direction != 3:
         if grid[y, x + 1] == "H":
             points += 1
+            Hbonds.append([[x, x + 1],[y, y]])
     if direction != 1:
         if grid[y, x - 1] == "H":
             points += 1
+            Hbonds.append([[x, x - 1],[y, y]])
     if direction != 2:
         if grid[y - 1, x] == "H":
             points += 1
+            Hbonds.append([[x, x],[y, y - 1]])
     if direction != 0:
         if grid[y + 1, x] == "H":
             points += 1
+            Hbonds.append([[x, x],[y, y + 1]])
     return points
 
+x = 0
 
-proteinplacer()
+while x < 10000:
+    
+    backupdirections = deepcopy(directions)
+    #backupscore = score
+    
+    newdirectionplace = randint(0,len(directions) - 1)
+    newdirection = randint(0,3)
+    directions[newdirectionplace] = newdirection    
+    
+    score = proteinchecker()
 
-function(len(directions) - 1)
+    if score < 0:
+        directions = backupdirections
+    else:
+        x += 1
+    
 
-for i in bestproteins:
-    print i['grid']
-    print "score: %i" %i['score']
-    print
+'''
+print and plot the protein(s)
+'''
 
-print "there are %i proteins with this score" %len(bestproteins)
 
 elapsed_time = time.time() - start_time
 
+print
 print "elapsed time: %f seconds" %elapsed_time
+print "this protein has a score of: %i" %bestproteins[0]['score']
+print "there are %i proteins with this score" %len(bestproteins)
+print
+
+color = {'H': 'lightgreen', 'P': 'lightblue'}
+for j in bestproteins:
+    linex = []
+    liney = []
+    
+    #for j in bestproteins:
+    for i in j['coordinates']:
+        plt.scatter(i['coordinate'][0],i['coordinate'][1], s=1000, c=color[i['letter']], zorder=2)
+        plt.annotate(i['letter'], (i['coordinate'][0],i['coordinate'][1]), zorder=2)
+        linex.append(i['coordinate'][0])
+        liney.append(i['coordinate'][1])
+    
+    for i in Hbonds:
+        plt.plot(i[0], i[1], zorder = 1, lw=3, c='red', ls='dotted')
+    
+    plt.plot(linex, liney, zorder=1, lw=3, c='black')
+    
+    plt.axis('off')
+    plt.show()
